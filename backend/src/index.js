944437f18fe475c87e970940520dc07ecfefccd4
kim-xie <18576648902@163.com>
2026-08-09 21:53:27 +0800

    fix: 数据库日志更新

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
+import { dirname, join, resolve } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
